cc3ccf9041853bff4fd6405d8abb95b264659066
Farah Warfa <fwarfa@gmail.com>
2021-08-14 16:42:28 -0500

    setup components, added images to gallery data, built logic for gallerylist and gallery item, built out logic for like button, working on toggle between image and description

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
 import './App.css';
+import GalleryList from './GalleryList/GalleryList';
+import GalleryItem from './GalleryItem/GalleryItem';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
 
 function App() {
+
+  let [galleryList, setGalleryList] = useState([]);
+
+  useEffect(() => {
+    getGallery();
+  }, []);
+
+  const getGallery = () => {
+    axios.get('/gallery')
+      .then(response => {
+        console.log('GET /gallery', response.data);
+
+        setGalleryList(response.data);
+      }).catch(error => {
+        console.log('GET /gallery error', error);
+      });
+  };
+
+  const likeButton = (id) => {
+    console.log('id is ', id);
+    axios.put(`/gallery/like/${id}`)
+      .then(response => {
+        console.log('PUT /like', response.data);
+        getGallery();
+      }).catch(error => {
+        console.log('PUT /like error', error);
+      });
+  };
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        < GalleryList 
+            galleryList = {galleryList}
+            onGetGallery = {getGallery}
+            likeButton = {likeButton}
+        />
       </div>
     );
 }

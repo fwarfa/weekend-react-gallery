a0571ba6e8dc015564e8e05928f73202881d9700
Farah Warfa <fwarfa@gmail.com>
2021-08-16 10:02:22 -0500

    moved gallery area to gallery list component, correct and created terinary to toggle image and description

--- src/components/App/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryItem/GalleryItem.jsx
@@ -1,52 +1,35 @@
 import {useState} from 'react';
 
-function GalleryItem ({onGetGallery, galleryList, likeButton}) {
+function GalleryItem ({onGetGallery, galleryList, likeButton, image}) {
 
-    // let [showImage, setShowImage] = useState(true);
+    let [showDescription, setShowDiscription] = useState(true);
 
-
-
-    // const handleClick = (id) => {
-
-    //     let galleryImage = document.getElementById(id);
-    //     let imgDescription = document.getElementById(id);
-    
-    //     if (galleryImage.style.visibility === "visible") {
-    //         galleryImage.style.visibility = "hidden";
-    //         imgDescription.style.visibility = "visible";
-    //         console.log('you clicked a description!');
-
-    //     } else {
-    //         galleryImage.style.visibility = "visible";
-    //         imgDescription.style.visibility = "hidden";
-    //         console.log('you clicked an image!');
-    //     }
-
-    // }
+    const handleClick = () => {
+        setShowDiscription(!showDescription);
+    }
 
     return (
         <>
-            {galleryList.map(image => {
-                return (
-
+            {
+                showDescription ? 
                     <div key= {image.id}>
                         <img src={image.path} 
                             alt={image.title} 
+                            id={image.id}
                             width="100" 
                             height="100"
-                            id={image.id}
-                            onClick ={() => {handleClick(image.id)}} // WIP
+                            onClick ={() => {handleClick()}}
                         />
-                        <br/>
-                        <span id={image.id}
-                        onClick ={() => {handleClick(image.id)}} // WIP
-                        >{image.description}</span>
-                        <br/>
-                        <button onClick={() => {likeButton(image.id)}}>Like</button>
-                        <h4>{image.likes} People like this</h4>
                     </div>
-                )
-            })}
+                    :
+                    <div 
+                        id={image.id}
+                        onClick ={() => {handleClick()}}
+                    >{image.description}
+                    </div>
+            }
+            <button onClick={() => {likeButton(image.id)}}>Like</button>
+            <h4>{image.likes} People like this</h4>
         </>
     );
 }
--- src/components/App/GalleryList/GalleryList.jsx
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -8,14 +8,17 @@ function GalleryList({onGetGallery, galleryList, likeButton}) {
     return (
         <>
             <h1>My Gallery</h1>
-            <div>
-                < GalleryItem 
-                    galleryList = {galleryList}
-                    onGetGallery = {onGetGallery}
-                    likeButton = {likeButton}
-                />
-            </div>
 
+            {galleryList.map(image => {
+                return (
+                    < GalleryItem 
+                        galleryList = {galleryList}
+                        onGetGallery = {onGetGallery}
+                        likeButton = {likeButton}
+                        image = {image}
+                    />
+                )
+            })}
         </>
     );
 }
